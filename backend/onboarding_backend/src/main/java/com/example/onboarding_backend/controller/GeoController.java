f5539e7ff2044b52b3fe248949a1605bcc5b278b
zihaowang <84835915+zwan0207@users.noreply.github.com>
2024-03-08 15:44:33 +1100

    add interface for sending data to front side

--- backend/onboarding_backend/src/main/java/com/example/onboarding_backend/controller/GeoController.java
+++ backend/onboarding_backend/src/main/java/com/example/onboarding_backend/controller/GeoController.java
@@ -3,12 +3,14 @@ package com.example.onboarding_backend.controller;
 import com.example.onboarding_backend.entity.Geo;
 import com.example.onboarding_backend.mapper.GeoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class GeoController {
     @Autowired
     private GeoMapper geoMapper;
